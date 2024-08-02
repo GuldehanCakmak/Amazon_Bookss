8dbfeb820e208cb663cce2bc708097c77e353601
GuldehanCakmak <guldehancakmak@gmail.com>
2024-08-02 16:50:36 +0200

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from wordcloud import WordCloud, STOPWORDS
