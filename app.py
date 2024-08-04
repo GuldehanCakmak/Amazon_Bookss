f894f21f4b0df7ebc353dd4088103e95bae7c43a
GuldehanCakmak <guldehancakmak@gmail.com>
2024-08-05 00:27:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(layout='wide', page_title='Book Recommender', page_icon='book
 
 # home tab
 home_tab, graph_tab, recommendation_tab = st.tabs(["Ana Sayfa", "Grafikler","Öneri Sistemi"])
-col1, col2, col3 = home_tab.columns([1,1,1)
+col1, col2, col3 = home_tab.columns([1,1,1])
 col1.image("https://www.looper.com/img/gallery/star-wars-how-darth-vaders-costume-limited-the-duel-in-a-new-hope/l-intro-1683252662.jpg")
 col1.subheader("Nedir?")
 col1.markdown("*Merhaba sevgili kitap severler! Ben bir kitap kurdu olarak her zaman yeni ve ilginç kitaplar keşfetmeyi, okumayı ve bu kitapları arkadaşlarımla paylaşmayı çok severim. Geçenlerde, Amazon'un devasa kitap veri tabanını keşfetmeye karar verdim. Amacım, arkadaşlarıma onların zevklerine en uygun kitapları önermek ve bu devasa bilgi denizinden en iyi şekilde faydalanmak oldu. İşte bu serüvenin hikayesi ve sonuçları!*")
