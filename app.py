9430ba26ae70474081a42c89974ef92463ba6fa2
GuldehanCakmak <guldehancakmak@gmail.com>
2024-08-04 17:35:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,21 +28,21 @@ meta, user = get_data()
 home_tab, graph_tab, recommendation_tab = st.tabs(["Ana Sayfa", "Grafikler","Öneri Sistemi"])
 
  # Verileri işleyin ve ölçekleyin
-    user = meta[['Price', 'Rating', 'No. of People Rated', 'Main Genre']]  # Özellik sütunlarını belirleyin
-    scaler = MinMaxScaler()
-    user_scaled = scaler.fit_transform(features)
+user = meta[['Price', 'Rating', 'No. of People Rated', 'Main Genre']]  # Özellik sütunlarını belirleyin
+scaler = MinMaxScaler()
+user_scaled = scaler.fit_transform(features)
 
     # NaN değerleri kaldırın
-    nan_mask = np.isnan(user_scaled).any(axis=1)
-    user_scaled = user_scaled[~nan_mask]
+nan_mask = np.isnan(user_scaled).any(axis=1)
+user_scaled = user_scaled[~nan_mask]
 
     # K-Means kümeleme
-    kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    cluster_labels = kmeans.fit_predict(user_scaled)
+kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)
+cluster_labels = kmeans.fit_predict(user_scaled)
 
     # PCA ile boyut indirgeme
-    pca = PCA(n_components=2)
-    user_pca = pca.fit_transform(user_scaled)
+pca = PCA(n_components=2)
+user_pca = pca.fit_transform(user_scaled)
 
 
 
