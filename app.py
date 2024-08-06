a2bdb68d6b279f1206e4cf7d7001fba555c86361
GuldehanCakmak <guldehancakmak@gmail.com>
2024-08-06 17:19:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ col2.markdown("*Sonuçlar gerçekten de heyecan vericiydi! Arkadaşlarıma kişi
 col2.markdown("*Ahmet, sen polisiye romanları çok seviyorsun. İşte bu yağmurlu gün için mükemmel bir öneri:  Sherlock Holmes serisi. Eminim ki seni çok heyecanlandıracak!*")
 col2.markdown("*Ayşe, senin için harika bir romantik kitap buldum. Hava güneşli ve senin de keyfin yerinde.  Pride and Prejudice tam sana göre!*")
 col2.markdown("*Mehmet, sesli kitapları sevdiğini biliyorum. İşte işe giderken dinleyebileceğin bir kitap:   Sapiens: İnsanlığın Kısa Tarihi . Eminim çok şey öğreneceksin.*")
-col2.image("https://i.pinimg.com/564x/ca/8f/dc/ca8fdc82586bea8fa7fff647ee37b4e1.jpg")
-
+#col2.image("https://i.pinimg.com/564x/ca/8f/dc/ca8fdc82586bea8fa7fff647ee37b4e1.jpg")
+col2.image("https://img.aydinlik.com.tr/rcman/Cw1280h720q95gc/storage/files/images/2023/07/11/amazonda-yapay-zeka-tarafindan-yazilan-kitaplar-yayginlasiyor-istila-mi-FHNP.jpg")
 col3.subheader("Günün Kitap Tavsiyesi")
 
 
